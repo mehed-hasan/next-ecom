bdf7cf023e781c43e314aeb4968ca9e32b147d08
mehed-hasan <mehedi.hasan@intelli.global>
2024-12-08 18:57:50 +0600

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "picsum.photos",
+      "flowbite.com",
+      "img.daisyui.com",
+      "monsta-demo.myshopify.com",
+      "themewagon.github.io",
+      "maraviyainfotech.com",
+      "plus.unsplash.com",
+      "pub-e63b17b4d990438a83af58c15949f8a2.r2.dev",
+    ],
+  },
 };
 
 export default nextConfig;
